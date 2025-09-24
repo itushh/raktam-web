8940cb87011c64b0c02a9e606419ea48aa11ae0f
itushh <tusharramgirkar99@gmail.com>
2025-09-24 20:24:05 +0530

    minor bug fix

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,16 @@ import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    host: true, // 👈 important: allows LAN access
-    port: 5173, // or whatever port you want
-  },
-  // Environment variable configuration
-  define: {
-    __DEV__: JSON.stringify(process.env.NODE_ENV === 'development'),
   },
 })
